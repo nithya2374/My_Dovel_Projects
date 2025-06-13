cac00438cff4590242613dcf42169f106f2f559d
Nithyasri R <128442903+nithya2374@users.noreply.github.com>
2025-06-13 17:50:42 +0530

    Delete Day_20 directory

--- Day_20/vite-project/vite.config.js
+++
@@ -1,9 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: 'Full_Stack_Dovel/', // 👈 Replace with your actual repo name
-})
-
